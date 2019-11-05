a81a15fe0b7148c1e75859cfa77e926a90af3c9e
sufferingcode <ybell1028@gmail.com>
2019-11-05 13:09:13 +0900

    BottomNavigationBar

---
+++ LayoutProto/app/src/main/java/org/techtown/layoutproto/MainActivity.java
@@ -0,0 +1,61 @@
+package org.techtown.layoutproto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // FrameLayout에 각 메뉴의 Fragment를 바꿔 줌
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    // 4개의 메뉴에 들어갈 Fragment들
+    private Menu1Fragment menu1Fragment = new Menu1Fragment();
+    private Menu2Fragment menu2Fragment = new Menu2Fragment();
+    private Menu3Fragment menu3Fragment = new Menu3Fragment();
+    private Menu4Fragment menu4Fragment = new Menu4Fragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        // 첫 화면 지정
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, menu1Fragment).commitAllowingStateLoss();
+
+        // bottomNavigationView의 아이템이 선택될 때 호출될 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.navigation_menu1: {
+                        transaction.replace(R.id.frame_layout, menu1Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.navigation_menu2: {
+                        transaction.replace(R.id.frame_layout, menu2Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.navigation_menu3: {
+                        transaction.replace(R.id.frame_layout, menu3Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                    case R.id.navigation_menu4: {
+                        transaction.replace(R.id.frame_layout, menu4Fragment).commitAllowingStateLoss();
+                        break;
+                    }
+                }
+
+                return true;
+            }
+        });
+    }
+}
---
+++ LayoutProto/app/src/main/java/org/techtown/layoutproto/Menu1Fragment.java
@@ -0,0 +1,18 @@
+package org.techtown.layoutproto;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Menu1Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu1, container, false);
+    }
+}
