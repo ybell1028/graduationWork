4fad2b8ebe840aa836ca1dd2f71eebb235b065da
Jong Hun Hwang <ybell1028@gmail.com>
2019-11-20 15:10:11 +0900

    spinner complited

--- LayoutProto/app/src/main/java/org/techtown/layoutproto/AddshopDialogFragment.java
+++ LayoutProto/app/src/main/java/org/techtown/layoutproto/AddshopDialogFragment.java
@@ -14,11 +14,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import java.util.ArrayList;
+
 public class AddshopDialogFragment extends DialogFragment implements View.OnClickListener {
 
     ShoppingFragment shoppingFragment;
-    ArrayAdapter<String> adapter;
-    Spinner spinner;
+    ArrayAdapter<String> adapter1, adapter2;
+    Spinner mallSpinner, syncSpinner;
 
     public AddshopDialogFragment() {
 
@@ -33,20 +35,44 @@ public class AddshopDialogFragment extends DialogFragment implements View.OnClic
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.shop_add_dialog, container);
 
-        spinner = (Spinner)view.findViewById(R.id.shopSpinner);
+        mallSpinner = (Spinner)view.findViewById(R.id.mallSpinner);
         //spinner item 가져오기
-        String[] str = getResources().getStringArray(R.array.mallList);
+        String[] str1 = getResources().getStringArray(R.array.mallList);
         //malllist.xml(item)과 str을 인자로 어댑터 생성
-        adapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_item, str);
+        adapter1 = new ArrayAdapter<String>(getContext(), R.layout.spinner_item, str1);
 
-        adapter.setDropDownViewResource(R.layout.spi);
-        spinner.setAdapter(adapter);
+        adapter1.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        mallSpinner.setAdapter(adapter1);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        mallSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(spinner.getSelectedItemPosition() > 0){
-                    Log.v("알림",spinner.getSelectedItem().toString()+ "is selected");
+                if(mallSpinner.getSelectedItemPosition() > 0){
+                    Log.v("알림",mallSpinner.getSelectedItem().toString()+ "is selected");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        syncSpinner = (Spinner)view.findViewById(R.id.syncSpinner);
+        //spinner item 가져오기
+        ArrayList str2 = new ArrayList<String>();
+        str2.add("사용");
+        str2.add("미사용");
+        adapter2 = new ArrayAdapter<String>(getContext(), R.layout.spinner_item, str2);
+
+        adapter2.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        syncSpinner.setAdapter(adapter2);
+
+        syncSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(syncSpinner.getSelectedItemPosition() > 0){
+                    Log.v("알림",syncSpinner.getSelectedItem().toString()+ "is selected");
                 }
             }
 
